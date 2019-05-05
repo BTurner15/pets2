fd2818c1b3186729e427daa3f664a3c75532bf3f
Bruce Turner <you@example.com>
2019-05-05 00:19:53 -0400

    Ok, I think it is adding up Sat. @09:19pm. It's about time

--- index.php
+++ index.php
@@ -2,23 +2,25 @@
 /**
  * Created by PhpStorm.
  * User: Bruce
- * Date: 04-26-2019
+ * Date: 04-26-2019 (creation)
  * Time: 7:31 PM
-
+ * Mods: 05-04 Sat.
  * Pair Programming #3 Pets3 */
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
-//Require autoload
+//Require autoload(s)
 require_once('vendor/autoload.php');
 require_once('model/validation-functions.php');
 
+session_start();
 //Create an instance of the Base Class
 $f3 = Base::instance();
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 //Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
 
+//make sure that these match what is in the form. Used for validation.
 $f3->set('colors', array('red', 'green', 'yellow', 'blue'));
 
 //Define a default route
@@ -30,6 +32,7 @@ $f3->route('GET /', function() {
 //Define a route that renders form1.html to get a valid animal type
 $f3->route('GET|POST /order', function($f3) {
     $_SESSION=array();
+    print_r($_POST);
     $template = new Template();
     //check if $POST even exists, then check animal type
     if (isset($_POST['animal'])) {
@@ -51,7 +54,9 @@ $f3->route('GET|POST /order', function($f3) {
 //Get the data from form1 and add it to a session variable
 //Display form2
 $f3->route('GET|POST /order2', function($f3) {
-     $template = new Template();
+    $template = new Template();
+    print_r($_POST);
+    print_r($_SESSION);
     if (isset($_POST['color'])) {
         //check valid color
         if (validColor($_POST['color'])) {
@@ -68,7 +73,8 @@ $f3->route('GET|POST /order2', function($f3) {
 });
 
 $f3->route('GET|POST /results', function() {
-    $_SESSION['color'] =    $_POST['color'];
+    print_r($_POST);
+    print_r($_SESSION);
     $view = new Template();
     echo $view->render('views/results.html');
 });
--- model/validation-functions.php
+++ model/validation-functions.php
@@ -1,18 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Bruce workng during tutoring with Jake in background.
+ * Working thru exercises in PP pets2 after class
  * User: Dracula
  * Date: 4/26/2019
- * Time: 2:28 PM
+ * Time: 2:28 PM; worked again on 05-04-Sat.
  */
 /* validate a color
  *
-    *@param String color
- * *return boolean
+ * @param String color
+ * return boolean
  * */
 function validColor($color)
 {
     global $f3;
+    print_r($_POST);
+    print_r($_SESSION);
     return in_array($color, $f3->get('colors'));
 }
 /* Validate a string
